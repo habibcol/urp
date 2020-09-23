088898a06f1d0ae4a70d2e634c694491c6685898
habibcol <habibur161725@gmail.com>
2020-09-24 01:02:37 +0600

    added animated logo

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
-document.addEventListener("mousemove", parallax);
-function parallax(e){
-    this.querySelectorAll('.layer').forEach(layer => {
-        const speed = layer.getAttribute('data-speed')
+const logo = document.querySelectorAll("#logo path");
 
-        const x = (window.innerWidth = e.pageX*speed)/100;
-        const y = (window.innerHeight = e.pageY*speed)/100;
-
-        layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
-    });
-} 
+for(let i = 0; i< logo.length; i++){
+    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+}
